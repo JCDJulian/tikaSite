657d83bfe139869057be3943409f467fd98ae7a9
Julian <julian.deocampo@gmail.com>
2015-03-11 12:03:43 -0700

    Tab toggling works. Just need to add pane content!

--- tabs.js
+++ tabs.js
@@ -11,7 +11,6 @@ $(document).ready(function(){
 	$("#banner").css({
 		"font-size":"200%",
 		"color":"#80577F",//purple
-		//"text-shadow":"-1px -1px 0 #000",
 		"font-family":"'Century Gothic', serif",
 		"font-weight":"400",
 		"margin-left":"5px",
@@ -52,11 +51,9 @@ $(document).ready(function(){
   		"top": "-10%",
 	});
 
-
-	$("div.container").append("<div class='home'></div>")
 	var remainingHeight = $("html").height() - ($("div.header").height()+$("#navbar").height());
-	console.log($("div.header").height()+$("#navbar").height());
-	console.log()
+
+	$("div.container").append("<div class='home'></div>");
 	$("div.home").css({
 		"width":"100%",
 		"height": remainingHeight,
@@ -67,25 +64,85 @@ $(document).ready(function(){
 		"background-size": "cover"
 	});
 
-	$("div.home").append("<p id='greeting'>Hello</br> world!</p>");
-	$("#greeting").css({
-		"position":"relative",
-		"top": "30%",
-		"left": "15%",
-		"width":"30px",
-		"font-size":"500%",
-		"color": "black",
-		"text-shadow":"-3px -3px 0 #fff",
-		"font-family":"Open Sans, sans-serif",
-		"font-weight":"600",
-		"margin":"0",
-		"padding":"0"
+		$("div.home").append("<p id='greeting'>Hello</br> world!</p>");
+		$("#greeting").css({
+			"position":"relative",
+			"top": "30%",
+			"left": "15%",
+			"width":"30px",
+			"font-size":"500%",
+			"color": "black",
+			"text-shadow":"3px 3px 0 #888",
+			"font-family":"Open Sans, sans-serif",
+			"font-weight":"600",
+			"margin":"0",
+			"padding":"0"
+		});
+
+	$("div.container").append("<div class='about'></div>")
+	$("div.about").css({
+		"width":"100%",
+		"height": remainingHeight,
+		"background-image": "url('images/about.jpg')",
+		"background-position": "center bottom",
+		"background-repeat":"no-repeat",
+		"background-color": "white",
+		"background-size": "cover"
 	});
 
-	$("header").hide();
-	$("header").fadeIn(2000);
+	$("div.container").append("<div class='resume'></div>")
+	$("div.resume").css({
+		"width":"100%",
+		"height": remainingHeight,
+		//"background-image": "url('images/about.jpg')",
+		"background-position": "center bottom",
+		"background-repeat":"no-repeat",
+		"background-size": "cover"
+	});
+
+	$("div.container").append("<div class='research'></div>")
+	$("div.research").css({
+		"width":"100%",
+		"height": remainingHeight,
+		"background-image": "url('images/research.jpg')",
+		"background-position": "center bottom",
+		"background-repeat":"no-repeat",
+		"background-color": "white",
+		"background-size": "cover"
+	});
+
+    $("div.about").hide();
+    $("div.resume").hide();
+    $("div.research").hide();
+	$("div.header").hide();
+	$("div.header").fadeIn(2000);
 	$("#navbar").hide();
 	$("#navbar").fadeIn(2000);
 	$("div.home").hide();
 	$("div.home").fadeIn(2000);
+
+	$("li").eq(0).click(function () {
+        $("div.home").fadeIn(1000);
+        $("div.about").hide();
+        $("div.resume").hide();
+        $("div.research").hide();
+    });
+    $("li").eq(1).click(function () {
+        $("div.home").hide();
+        $("div.about").fadeIn(1000);
+        $("div.resume").hide();
+        $("div.research").hide();
+    });
+    $("li").eq(2).click(function () {
+        $("div.home").hide();
+        $("div.about").hide();
+        $("div.resume").fadeIn(1000);
+        $("div.research").hide();
+    });
+    $("li").eq(3).click(function () {
+        $("div.home").hide();
+        $("div.about").hide();
+        $("div.resume").hide();
+        $("div.research").fadeIn(1000);
+    });
 });
